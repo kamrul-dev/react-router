647c3b191dc253a566d26dbed641ce2dd7f0f23d
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-29 02:04:46 +0600

    Nested route with useEffect dependency Injection

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import Friends from './components/Friends/Friends';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
+import Post from './components/Post/Post';
+import PostDetail from './components/PostDetail/PostDetail';
 
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>    
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/friends" element={<Friends />}></Route>
         <Route path="/friend/:friendId" element={<FriendDetail></FriendDetail>}></Route>
+        <Route path="/posts" element={<Post></Post>}>
+          <Route path=':postId' element={<PostDetail></PostDetail>}></Route>
+        </Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
