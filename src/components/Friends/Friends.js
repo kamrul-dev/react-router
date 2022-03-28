d6f51a1a4c3e490b5645536b1d08625eaf8b20de
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-28 22:42:33 +0600

    load users data

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Friends = () => {
+    const [friends, setFriends] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setFriends(data));
+    }, []);
     return (
         <div>
             <h2>Hello Friends. How are you?</h2>
-            <p><small>Give me some money..</small></p>
+            <p><small>Give me some money.. {friends.length}</small></p>
         </div>
     );
 };
